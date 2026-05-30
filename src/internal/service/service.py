ad4f7d6f3e421012cab9dc6493d45811a6c9e372
Штейнберг Алиса <aesteynberg@edu.hse.ru>
2026-05-31 00:41:12 +0500

    Add service initialization

--- src/internal/service/service.py
+++ src/internal/service/service.py
@@ -5,4 +5,10 @@ from ..regexp.base_regexp import BaseRegExp
 
 class Service(BaseService):
     DEFAULT_CATEGORY = "Без категории"
-    
+
+    def __init__(self, category_reg_exps):
+        self.category_reg_exps = category_reg_exps
+
+    @property
+    def category_reg_exps(self):
+        return self._category_reg_exps
