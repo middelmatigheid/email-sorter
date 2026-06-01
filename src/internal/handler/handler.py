367b1907877beb42fa225e13f358f030580ed3dd
mazoxa07 <mazoxhin07@gmail.com>
2026-06-01 20:58:21 +0000

    Realized handler.py handler_test.py

--- src/internal/handler/handler.py
+++ src/internal/handler/handler.py
@@ -2,27 +2,41 @@ import os
 import shutil
 from .base_handler import BaseHandler
 from ..service.base_service import BaseService
+from ..metrics.base_metrics import BaseMetrics
+from ..logger.base_logger import BaseLogger
 
 class Handler(BaseHandler):
-    def __init__(self, input_folder, output_folder, service, metrics):
+    def __init__(self, input_folder, output_folder, service, metrics, logger=None):
         if not isinstance(service, BaseService):
             raise ValueError("Handler's service should implement class BaseService")
+        if not isinstance(metrics, BaseMetrics):
+            raise ValueError("Handler's metrics should implement class BaseMetrics")
+        if not isinstance(logger, BaseLogger):
+            raise ValueError("Handler's logger should implement class BaseLogger")
+        self.input_folder = input_folder
         self.input_folder = input_folder
         self.output_folder = output_folder
         self.service = service
         self.metrics = metrics
+        self.logger = logger
 
     def handle(self, file_name):
+        if not os.path.exists(f"{self.input_folder}/{file_name}"):
+            if self.logger:
+                self.logger.error(f"File '{file_name}' doesn't exist")
+            raise ValueError(f"Specified path '{self.input_folder}/{file_name}' doesn't exist")
+        
         if file_name.split(".")[-1] != "txt":
+            if self.logger:
+                self.logger.error(f"File '{file_name}' is not an email")
             self.metrics.add_to_category("Не письма")
             shutil.move(f"{self.input_folder}/{file_name}", f"{self.output_folder}/Не письма/{file_name}")
             return
-
-        if not os.path.exists(f"{self.input_folder}/{file_name}"):
-            raise ValueError(f"Specified path '{self.input_folder}/{file_name}' doesn't exist")
         
         with open(f"{self.input_folder}/{file_name}", "r", encoding="utf-8") as file:
             text = file.read()
             category = self.service.get_category(text)
+        if self.logger:
+            self.logger.info(f"File '{file_name}' moved to category '{category}'")
         self.metrics.add_to_category(category)
         shutil.move(f"{self.input_folder}/{file_name}", f"{self.output_folder}/{category}/{file_name}")
---
+++ test/internal/handler/handler_test.py
@@ -0,0 +1,95 @@
+import os
+import shutil
+import pytest
+from internal.service.base_service import BaseService
+from internal.metrics.base_metrics import BaseMetrics
+from internal.logger.base_logger import BaseLogger
+from internal.handler.handler import Handler
+
+class MockService(BaseService):
+    def __init__(self):
+        pass
+
+    def get_category(self, text):
+        return "category"
+    
+class MockMetrics(BaseMetrics):
+    def __init__(self):
+        pass
+
+    def add_to_category(self, category):
+        pass
+
+    def get_metrics(self):
+        pass
+
+class MockLogger(BaseLogger):
+    def __init__(self):
+        pass
+
+    def info(self, text):
+        pass
+
+    def error(self, text):
+        pass
+
+def test_not_service():
+    with pytest.raises(ValueError, match="Handler's service should implement class BaseService"):
+        handler = Handler("input_test", "output_test", "service", "metrics", "logger")
+
+def test_not_metrics():
+    service = MockService()
+    with pytest.raises(ValueError, match="Handler's metrics should implement class BaseMetrics"):
+        handler = Handler("input_test", "output_test", service, "metrics", "logger")
+
+def test_not_logger():
+    service = MockService()
+    metrics = MockMetrics()
+    with pytest.raises(ValueError, match="Handler's logger should implement class BaseLogger"):
+        handler = Handler("input_test", "output_test", service, metrics, "logger")
+
+def test_handle_file_doesnt_exist():
+    service = MockService()
+    metrics = MockMetrics()
+    logger = MockLogger()
+    handler = Handler("input_test", "output_test", service, metrics, logger)
+    with pytest.raises(ValueError, match=f"Specified path 'input_test/unknown.txt' doesn't exist"):
+        handler.handle("unknown.txt")
+
+def test_handle_file_not_txt():
+    service = MockService()
+    metrics = MockMetrics()
+    logger = MockLogger()
+    handler = Handler("input_test", "output_test", service, metrics, logger)
+    if not os.path.exists("input_test"):
+        os.mkdir("input_test")
+    with open("input_test/not_mail.json", "w", encoding="utf-8") as file:
+        file.write("")
+    if not os.path.exists("output_test"):
+        os.mkdir("output_test")
+    if not os.path.exists("output_test/Не письма"):
+        os.mkdir("output_test/Не письма")
+    handler.handle("not_mail.json")
+    if not os.path.exists("output_test/Не письма/not_mail.json"):
+        assert False
+    shutil.rmtree("input_test")
+    shutil.rmtree("output_test")
+
+def test_handle():
+    service = MockService()
+    metrics = MockMetrics()
+    logger = MockLogger()
+    handler = Handler("input_test", "output_test", service, metrics, logger)
+    if not os.path.exists("input_test"):
+        os.mkdir("input_test")
+    with open("input_test/mail.txt", "x", encoding="utf-8") as file:
+        file.write("")
+    if not os.path.exists("output_test"):
+        os.mkdir("output_test")
+    if not os.path.exists("output_test/category"):
+        os.mkdir("output_test/category")
+    handler.handle("mail.txt")
+    if not os.path.exists("output_test/category/mail.txt"):
+        assert False
+    shutil.rmtree("input_test")
+    shutil.rmtree("output_test")
